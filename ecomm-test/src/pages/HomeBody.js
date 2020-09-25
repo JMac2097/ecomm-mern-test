1d3acf2dc40be571d004fe08da23d394311d7bba
John McKie <john.mckie@remarkable.net>
2020-09-25 08:56:52 +0100

    body set up

---
+++ ecomm-test/src/pages/HomeBody.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomeBody = () => {
+
+    return (
+        <div>
+            <p>This is a the homepage body section</p>
+        </div>
+    )
+}
+
+export default HomeBody;
